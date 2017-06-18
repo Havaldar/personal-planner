05e4273d2a1b8cba1828e5cc9603df675aa473fe
Abhinav Havaldar <abhinav@Frames-MacBook-Pro.local>
2017-06-18 19:50:11 -0400

    whoops

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from twilio.rest import Client
 from os.path import join, dirname
 from dotenv import get_variables
 
-AIRTABLE_BASE_URL = "https://api.airtable.com/v0"
-AIRTABLE_BASE_ID = "app2tEnrXO9tQxei0"
-AIRTABLE_API_KEY = "keyRfbMDzIzUtCuPs"
-AIRTABLE_TABLE_NAME = "To Do"
 
 dotenv_path = join(dirname(__file__), '.env')
 env = get_variables(dotenv_path)
